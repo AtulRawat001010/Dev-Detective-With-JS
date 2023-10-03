b7cb87782a878a7e433606656bb1a8aac38fe4f7
AtulRawat <workerhere12@gmail.com>
2023-10-03 16:55:18 +0530

    #

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let url = "https://api.github.com/users/";
 
+let wrapper = document.querySelector(".wrapper");
 let submitBtn = document.querySelector(".searchBtn");
 let input = document.querySelector("input");
 let searchForm = document.querySelector(".searchForm");
@@ -10,10 +11,66 @@ let enableDark = document.querySelector(".modeBtn2");
 
 let appMode = document.querySelector(".app");
 
+let user_details = document.querySelector(".user-details")
+
 let appdarkMode = false;
 
-start();
 
+function render(data) {
+    if(data.message !== "Not Found"){
+    let user_img = document.querySelector(".user-img");
+    let user_name = document.querySelector(".user-name");
+    let user_id = document.querySelector(".user-id");
+    let user_desc = document.querySelector(".user-desc");
+    let user_joined = document.querySelector(".user-joined");
+    let user_repos_data = document.querySelector(".repos-data");
+    let user_followers_data = document.querySelector(".followers-data");
+    let user_following_data = document.querySelector(".following-data");
+    let user_location = document.querySelector(".location");
+    let user_page = document.querySelector(".community");
+    let user_twitter = document.querySelector(".twitter");
+    let user_company = document.querySelector(".company");
+
+    user_img.src = `${data.avatar_url}`;
+    
+    if (`${data.bio}` !== "null"){
+        user_desc.innerText = `${data.bio}`;
+    }
+    else{
+        user_desc.innerText = "this user has no bio";
+    }
+
+    user_name.innerText = `${data.login}`;
+    user_id.innerText = `@${data.login}`;
+    user_id.href = `${data.html_url}`;
+    user_joined.innerText = `${data.created_at.split("T").shift()}`;
+
+    user_repos_data.innerText = `${data.public_repos
+    }`
+    user_followers_data.innerText = `${data.followers}`
+    user_following_data.innerText = `${data.following}`
+    
+    user_location.innerText = data.location == null ? "No location" : `${data.location}`;
+    user_page.innerText = data.blog == "" ? "No Page" : `${data.blog}`;
+    user_page.href = `${data.blog}`;
+    user_twitter.innerText = data.twitter_username == null ? "No Twitter" : `${data.twitter_username}`;
+    user_twitter.href = `https://twitter.com/${data.twitter_username}`
+    user_company.innerText = data.company == null ? "No Company" : `${data.company}`;
+    }
+
+    else{
+        console.log("hello annonymous");
+
+        let notFound = document.querySelector(".notFound");
+        notFound.classList.add("notFoundClass");
+        wrapper.classList.add("deactive");
+    }
+
+    
+}
+
+
+start();
 function start() {
     if(localStorage.dark_mode){
         darkMode();
@@ -22,6 +79,8 @@ function start() {
     if(localStorage.light_mode){
         lightMode();
     }
+
+    fetchData(input.value = "atulrawat001010");
 }
 
 
@@ -38,34 +97,13 @@ async function fetchData() {
             let res = await fetch(fetchData);
             let data = await res.json();
             console.log(data);
+
+            render(data);
         }catch (error) {
             console.log("this Error occured at fetchdata Function", error);
         }
     }
 }
-
-// async function searchUser() {
-//     if (!input.value == "") {
-//         console.log(input.value);
-//         try {
-//             let gitUrl = (url + input.value);
-//             let res = await fetch(gitUrl);
-
-//             let data = await res.json();
-//             console.log(data);
-//         }
-        
-//         catch (error) {
-//             console.log("Error is here", error);
-//         }
-//     }
-
-//     else{
-//         console.log("nahi chalra hai");
-//     }
-// }
-
-
 
 
 enableDark.addEventListener('click', (()=>{
